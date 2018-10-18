d8e737da3ea7a645403bb2910b9e724820063a1c
osteoblast22 <osteoblast22@gmail.com>
2018-10-18 17:31:45 +0300

    fix menucommand on violentmonkey

--- HandyImage-feedback.user.js
+++ HandyImage-feedback.user.js
@@ -4,6 +4,8 @@
 // @author      osteoblast22
 // @homepage    https://github.com/osteoblast22/HandyImage-feedback
 // @supportURL  https://github.com/osteoblast22/HandyImage-feedback/issues
+// @updateURL   https://github.com/osteoblast22/HandyImage-feedback/raw/master/HandyImage-feedback.user.js
+// @downloadURL https://github.com/osteoblast22/HandyImage-feedback/raw/master/HandyImage-feedback.user.js
 // @include http://*/*
 // @include https://*/*
 // @grant        GM_registerMenuCommand
@@ -14,66 +16,66 @@
 // @grant        GM_setValue
 // @grant        GM_notification
 // ==/UserScript==
-(function() {
-    GM_config.init({
-        'id': 'settings',
-        'fields': {
-            'username': {
-                'label': 'github username',
-                'type': 'text',
-            },
-            'password': {
-                'label': 'github passowrd or access token',
-                'type': 'text',
-            }
-        }
-    });
 
-    function PostSite() {
-        if (GM_config.get('username') == '' || GM_config.get('password') == '') {
-            GM_notification({
-                text: "please set your github credentials in the userscript settings",
-                title: "no credentials",
-                onclick: settingConfig
-            })
-            //alert("please set your github credentials in the userscript settings")
-        } else {
-            console.log(GM_config.get('username'))
-            console.log(GM_config.get('password'))
-            var postData = {
-                title: "add/fix " + window.location.host,
-                body: "sample: " + window.location.href + "\n\
+GM_registerMenuCommand('Handy Image feedback: github settings', settingConfig,"c")
+GM_registerMenuCommand('Handy Image feedback: Submit this site', PostSite,"P")
+
+GM_config.init({
+    'id': 'settings',
+    'fields': {
+        'username': {
+            'label': 'github username',
+            'type': 'text',
+        },
+        'password': {
+            'label': 'github passowrd or access token',
+            'type': 'text',
+        }
+    }
+});
+
+function PostSite() {
+    if (GM_config.get('username') == '' || GM_config.get('password') == '') {
+        GM_notification({
+            text: "please set your github credentials in the userscript settings",
+            title: "no credentials",
+            onclick: settingConfig
+        })
+        //alert("please set your github credentials in the userscript settings")
+    } else {
+        console.log(GM_config.get('username'))
+        console.log(GM_config.get('password'))
+        var postData = {
+            title: "add/fix " + window.location.host,
+            body: "sample: " + window.location.href + "\n\
 ---\n\
 <sub>posted by [HandyImage-feedback](https://github.com/osteoblast22/HandyImage-feedback)</sub>"
-            }
-            GM_xmlhttpRequest({
-                url: "https://api.github.com/repos/Owyn/HandyImage/issues",
-                method: "POST",
-                headers: {
-                    "Content-Type": "text/json",
-                    "Authorization": "Basic " + btoa(GM_config.get('username') + ":" + GM_config.get('password'))
-                },
-                user: GM_config.get('username'),
-                password: GM_config.get('password'),
-                data: JSON.stringify(postData),
-
-                onload: function(response) {
-                    if (response.status == 201) {
-                        GM_notification({
-                            text: "the site has been submited",
-                            title: "success"
-                        })
-                    } else {
-                        console.log(response)
-                    }
-                }
-            })
         }
-    }
+        GM_xmlhttpRequest({
+            url: "https://api.github.com/repos/Owyn/HandyImage/issues",
+            method: "POST",
+            headers: {
+                "Content-Type": "text/json",
+                "Authorization": "Basic " + btoa(GM_config.get('username') + ":" + GM_config.get('password'))
+            },
+            user: GM_config.get('username'),
+            password: GM_config.get('password'),
+            data: JSON.stringify(postData),
 
-    function settingConfig() {
-        GM_config.open();
+            onload: function(response) {
+                if (response.status == 201) {
+                    GM_notification({
+                        text: "the site has been submited",
+                        title: "success"
+                    })
+                } else {
+                    console.log(response)
+                }
+            }
+        })
     }
-    GM_registerMenuCommand('Handy Image feedback: github settings', settingConfig, 's')
-    GM_registerMenuCommand('Handy Image feedback: Submit this site', PostSite, 's')
-})();
+}
+
+function settingConfig() {
+    GM_config.open();
+}
