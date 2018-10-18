ae7116ce87a4f02c19ceb6b2f2c10818233b2fd8
osteoblast22 <osteoblast22@gmail.com>
2018-10-18 11:22:48 +0300

    first commit

---
+++ HandyImage-feedback.user.js
@@ -0,0 +1,79 @@
+// ==UserScript==
+// @name         Handy Image feedback
+// @version      0.1
+// @author      osteoblast22
+// @homepage    https://github.com/osteoblast22/HandyImage-feedback
+// @supportURL  https://github.com/osteoblast22/HandyImage-feedback/issues
+// @include http://*/*
+// @include https://*/*
+// @grant        GM_registerMenuCommand
+// @grant        GM_xmlhttpRequest
+// @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
+// @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_notification
+// ==/UserScript==
+(function() {
+    GM_config.init({
+        'id': 'settings',
+        'fields': {
+            'username': {
+                'label': 'github username',
+                'type': 'text',
+            },
+            'password': {
+                'label': 'github passowrd or access token',
+                'type': 'text',
+            }
+        }
+    });
+
+    function PostSite() {
+        if (GM_config.get('username') == '' || GM_config.get('password') == '') {
+            GM_notification({
+                text: "please set your github credentials in the userscript settings",
+                title: "no credentials",
+                onclick: settingConfig
+            })
+            //alert("please set your github credentials in the userscript settings")
+        } else {
+            console.log(GM_config.get('username'))
+            console.log(GM_config.get('password'))
+            var postData = {
+                title: "add/fix " + window.location.host,
+                body: "sample: " + window.location.href + "\n\
+---\n\
+<sub>posted by [HandyImage-feedback](https://github.com/osteoblast22/HandyImage-feedback)</sub>"
+            }
+            GM_xmlhttpRequest({
+                url: "https://api.github.com/repos/Owyn/HandyImage/issues",
+                method: "POST",
+                headers: {
+                    "Content-Type": "text/json",
+                    "Authorization": "Basic " + btoa(GM_config.get('username') + ":" + GM_config.get('password'))
+                },
+                user: GM_config.get('username'),
+                password: GM_config.get('password'),
+                data: JSON.stringify(postData),
+
+                onload: function(response) {
+                    if (response.status == 201) {
+                        GM_notification({
+                            text: "the site has been submited",
+                            title: "success"
+                        })
+                    } else {
+                        console.log(response)
+                    }
+                }
+            })
+        }
+    }
+
+    function settingConfig() {
+        GM_config.open();
+    }
+    GM_registerMenuCommand('Handy Image feedback: github settings', settingConfig, 's')
+    GM_registerMenuCommand('Handy Image feedback: Submit this site', PostSite, 's')
+})();
---
+++ README.md
@@ -0,0 +1,13 @@
+## What is this userscript for ?
+
+This userscript is used to simplify reporting broken sites to [HandyImage's](https://github.com/Owyn/HandyImage/) issue page
+
+### How to set it up
+
+First you need a github access token, head over to [github tokens](https://github.com/settings/tokens) and create an access token, name it anything you want,give it only one scope "public_repo"
+Then install the userscript as usual, from the tampermonkey menu choose "Handy Image feedback: github settings" here you enter your github username and access token
+
+### How to use it
+
+While you browse the internet if you come across an image hosting site that seems not supported by ImageHandy just open the tampermonkey menu and click "Handy Image feedback: Submit this site"
+this will report the site to the issues page of HandyImage, the url of the page you're in will be sent as a sample image for Owyn to work on, so make sure your on the right page when you click the button
